59fa01b7f6dd5795a1e76e1cef34c340585e4633
Evan Genest <genest.1@osu.edu>
2017-12-29 14:58:52 -0600

    Invoke an instance of router.  Make a single GET route.  At the last line, export this whole file so that the router is available to the main script.

--- fridayOnly.js
+++ fridayOnly.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const { seedElements } = require('./utils');
 
-const expressions
+const expressionsRouter = express.Router();
 
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+
+// Get all expressions
+expressionsRouter.get('/', (req, res, next) => {
+  res.send(expressions);
+});
+
+module.exports(expressionsRouter );
