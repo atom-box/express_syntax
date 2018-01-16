eae5f52a58a2f8fedf06321170f0f8520dbde6ce
Evan Genest <genest.1@osu.edu>
2018-01-16 07:11:19 -0600

    Write 2nd 3rd of the calls.

--- eRote.js
+++ eRote.js
@@ -11,8 +11,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
 app.use(express.static('public'));
+const PORT = process.env.PORT || 4002;
 
 const jellybeanBag = {
 	redPop: {
@@ -33,12 +33,31 @@ const jellybeanBag = {
 };
 
 app.use(morgan('tiny'));
-app.use(bodyParser);
+app.use(bodyParser.json() );
+app.use('/beans/:beanName', (req, res, next)=> {
+	 const beanName =req.params.beanName
+} )
+/* WOW -- app.use(SOME-URL!! )    */
 
-
-app.get('/beans/', )
+app.get('/beans/', (req, res, next)=> {
+	res.send(jellybeanBag);
+	/*OOPS!  DON'T 'return' ANYTHING.*/
+} );
 // RETURNS ALL BEANS AS OBJECT
-app.post('/beans/')
+
+app.post('/beans/', (req, res, next)=> {
+	const body = req.body;
+	const beanName = body.name;
+	if (jellybeanBag[beanName] || jellybeanBag[beanName] === 0){
+		return res.status(400).send('Extant!');
+	}
+	const numberOfBeans = Number(body.number) || 0;
+	jellybeanBag[beanName] = {
+		number: numberOfBeans;
+	};
+	res.send(jellybeanBag[beanName]);
+} );
+
 // SOME EXISTING BEAN, ALTER THE NUMBER
 app.get('/beans/:beanName',   )
 // JUST BRING INFO ON MY FAVE BEAN
@@ -49,5 +68,5 @@ app.post('/beans/:beanName/remove')
 app.delete('/beans/:beanName')
 // DISCONTINUE THAT FLAVOR
 app.listen( PORT, ()=> {
-	console.log("hey we're the monkeys.")
+	console.log(`Listening on Port ${PORT}.`)
 });
